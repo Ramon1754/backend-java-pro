5c1c19bcabf7b94ace61b59cce15bb2094b46ac9
Ramon Migueis <114538472+Ramon1754@users.noreply.github.com>
2024-08-20 11:21:31 -0300

    Create CategoriesApplication.java

---
+++ modulo-15-aula-2/memelandia/src/main/java/br/com/ebac/memelandia/CategoriesApplication.java
@@ -0,0 +1,11 @@
+package br.com.ebac.memelandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CategoriesApplication.class, args);
+    }
+}

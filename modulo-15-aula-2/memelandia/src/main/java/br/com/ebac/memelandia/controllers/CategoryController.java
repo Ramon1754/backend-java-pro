396d81fc0893de76ffffe4fba5a1bafa4c6905e0
Ramon Migueis <114538472+Ramon1754@users.noreply.github.com>
2024-08-20 11:22:28 -0300

    Create CategoryController.java

---
+++ modulo-15-aula-2/memelandia/src/main/java/br/com/ebac/memelandia/controllers/CategoryController.java
@@ -0,0 +1,35 @@
+package br.com.ebac.memelandia.controllers;
+
+import com.exemplo.categories.model.Category;
+import com.exemplo.categories.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+}
